88361f6ecaa4aec997ad2e1dd0c1949ab1cd5c9c
Andrei <matyshenko-1989@tut.by>
2022-11-07 18:35:35 +0300

    three commit

--- viceverse/viceverse/views.py
+++ viceverse/viceverse/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def reverse(request):
+    text = (request.GET['usertext'])
+    reveres_text = text[::-1]
+    return render(request, 'reverse.html', {'end_text': reveres_text, 'original_text': text})
