92666af850e5d0a1d34cfc22406fb848b09dd6a0
Andrei <matyshenko-1989@tut.by>
2022-11-07 18:46:35 +0300

    4 commit

--- viceverse/viceverse/views.py
+++ viceverse/viceverse/views.py
@@ -5,5 +5,6 @@ def home(request):
     return render(request, 'home.html')
 def reverse(request):
     text = (request.GET['usertext'])
+    number = len(text.split())
     reveres_text = text[::-1]
-    return render(request, 'reverse.html', {'end_text': reveres_text, 'original_text': text})
+    return render(request, 'reverse.html', {'end_text': reveres_text, 'original_text': text, 'number_text': number})
